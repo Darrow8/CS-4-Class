b6dde587afb3e47d4d705d12b93ef426aa3cb553
Darrow8 <darrowrhartman@gmail.com>
2021-10-09 14:18:27 -0700

    did forms stuff

--- forms/app.py
+++ forms/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__) #this has 2 underscores on each side
 
 @app.route('/')
 def hello_world():
-   return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    inputName = request.form.getList("inputName")
+    return render_template('results.html', inputName=inputName)
